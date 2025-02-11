ef05b0bf995acfbd5ccc9954bbfc6a3d3c96f3e7
tonyyph <tonyphvincent@gmail.com>
2025-02-11 23:09:36 +0700

    chore: tabbar animated hide/show

--- components/common/tab-bar.tsx
+++ components/common/tab-bar.tsx
@@ -9,18 +9,25 @@ import {
   UserCircle
 } from "lucide-react-native";
 import { rem } from "nativewind";
-import { Pressable, type PressableProps, View } from "react-native";
+import {
+  Pressable,
+  type PressableProps,
+  View,
+  TouchableWithoutFeedback
+} from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withTiming
 } from "react-native-reanimated";
+import { useEffect, useRef } from "react";
 
 type TabBarItemProps = {
   focused: boolean;
   icon: LucideIcon;
   descriptor: BottomTabBarProps["descriptors"][string];
 };
+
 function TabBarItem({
   icon: Icon,
   focused,
@@ -33,7 +40,7 @@ function TabBarItem({
       accessibilityRole="button"
       accessibilityState={focused ? { selected: true } : {}}
       accessibilityLabel={options.tabBarAccessibilityLabel}
-      className={"h-14 w-14 items-center justify-center"}
+      className={"h-12 w-12 items-center justify-center"}
       hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
       {...props}
     >
@@ -54,63 +61,97 @@ const TAB_BAR_ICONS = {
   profile: UserCircle
 };
 
-const TAB_BAR_ITEM_WIDTH = (3.5 + 1) * rem.get();
+const TAB_BAR_ITEM_WIDTH = (3 + 1) * rem.get();
 
 export function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
   const tabIndicatorPosition = useSharedValue(state.index * TAB_BAR_ITEM_WIDTH);
+  const tabBarTranslateY = useSharedValue(100); // Mặc định TabBar bị đẩy xuống ngoài màn hình
+  const hideTimer = useRef<NodeJS.Timeout | null>(null);
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ translateX: tabIndicatorPosition.value }]
   }));
 
+  const tabBarStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateY: withTiming(0, { duration: 300 }) }
+      // { translateY: withTiming(tabBarTranslateY.value, { duration: 300 }) }
+    ]
+  }));
+
+  console.log("tabBarTranslateY.value", tabBarTranslateY.value);
+  function showTabBar() {
+    tabBarTranslateY.value = 0;
+
+    if (hideTimer.current) clearTimeout(hideTimer.current);
+    hideTimer.current = setTimeout(() => {
+      tabBarTranslateY.value = 100;
+    }, 3000);
+  }
+
+  useEffect(() => {
+    hideTimer.current = setTimeout(() => {
+      tabBarTranslateY.value = 100;
+    }, 3000);
+
+    return () => {
+      if (hideTimer.current) clearTimeout(hideTimer.current);
+    };
+  }, []);
+
   return (
-    <View className="absolute bottom-9 flex-row items-center justify-center gap-3 self-center rounded-2xl border border-border bg-background p-2">
+    <View className="bg-transparent">
       <Animated.View
-        style={[animatedStyle]}
-        className="absolute left-2 h-14 w-14 rounded-xl bg-primary"
-      />
-      <View className="flex-row items-center gap-4">
-        {state.routes.map((route, index) => {
-          function onPress() {
-            Haptics.selectionAsync();
+        style={[tabBarStyle]}
+        className="absolute bottom-9 flex-row items-center justify-center gap-3 self-center rounded-2xl border border-border bg-background p-2"
+      >
+        <Animated.View
+          style={[animatedStyle]}
+          className="absolute left-2 h-12 w-12 rounded-xl bg-primary"
+        />
+        <View className="flex-row items-center gap-4">
+          {state.routes.map((route, index) => {
+            function onPress() {
+              Haptics.selectionAsync();
 
-            tabIndicatorPosition.value = withTiming(
-              index * TAB_BAR_ITEM_WIDTH,
-              {
-                duration: 300
+              tabIndicatorPosition.value = withTiming(
+                index * TAB_BAR_ITEM_WIDTH,
+                { duration: 300 }
+              );
+
+              const event = navigation.emit({
+                type: "tabPress",
+                target: route.key,
+                canPreventDefault: true
+              });
+
+              if (state.index !== index && !event.defaultPrevented) {
+                navigation.navigate(route.name, route.params);
               }
-            );
 
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true
-            });
-
-            if (state.index !== index && !event.defaultPrevented) {
-              navigation.navigate(route.name, route.params);
+              showTabBar();
             }
-          }
 
-          function onLongPress() {
-            navigation.emit({
-              type: "tabLongPress",
-              target: route.key
-            });
-          }
+            function onLongPress() {
+              navigation.emit({
+                type: "tabLongPress",
+                target: route.key
+              });
+            }
 
-          return (
-            <TabBarItem
-              key={route.key}
-              icon={TAB_BAR_ICONS[route.name as keyof typeof TAB_BAR_ICONS]}
-              focused={state.index === index}
-              descriptor={descriptors[route.key]}
-              onPress={onPress}
-              onLongPress={onLongPress}
-            />
-          );
-        })}
-      </View>
+            return (
+              <TabBarItem
+                key={route.key}
+                icon={TAB_BAR_ICONS[route.name as keyof typeof TAB_BAR_ICONS]}
+                focused={state.index === index}
+                descriptor={descriptors[route.key]}
+                onPress={onPress}
+                onLongPress={onLongPress}
+              />
+            );
+          })}
+        </View>
+      </Animated.View>
     </View>
   );
 }
