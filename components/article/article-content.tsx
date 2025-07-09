de3472f65be7abb7a15fc5d48f016a520c395edb
tonyyph <tonyphvincent@gmail.com>
2025-07-09 16:30:32 +0700

    update code

--- components/article/article-content.tsx
+++ components/article/article-content.tsx
@@ -2,17 +2,11 @@ import { useMemoFunc } from "@/hooks";
 import { cn } from "@/lib/utils";
 import { SCREEN_HEIGHT } from "@/utils";
 import { useState } from "react";
-import {
-  LayoutChangeEvent,
-  Text,
-  useWindowDimensions,
-  View
-} from "react-native";
+import { LayoutChangeEvent, Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
 export const ArticleContent = ({ content }: { content: any }) => {
   const [showReadMore, setShowReadMore] = useState(false);
-  const { height } = useWindowDimensions();
 
   const handleLayout = useMemoFunc((event: LayoutChangeEvent) => {
     const { height } = event.nativeEvent.layout;
@@ -29,7 +23,6 @@ export const ArticleContent = ({ content }: { content: any }) => {
       scrollEnabled={showReadMore}
     >
       <View
-        style={{ maxHeight: height }}
         className={cn(`overflow-hidden flex-row flex-wrap`)}
         onLayout={handleLayout}
       >
